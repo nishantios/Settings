2f823e448a2fa033aea61569aa3b29e0b5fbbd70
Nishant <nishantminerva@gmail.com>
2024-03-14 12:49:01 +0530

    Initial Commit

---
+++ Settings UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Settings UIKit
+//
+//  Created by Nishant Minerva on 14/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
